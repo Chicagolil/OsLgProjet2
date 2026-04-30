c2c985e5f63791e59e552acac23cc0a80e105fac
Chicagolil-ulg <Lilian.Devroye@student.uliege.be>
2026-04-30 17:47:43 +0000

    faut nettoyer tout ça

--- Challenge3/ciphered/src/prog.bpf.c
+++ Challenge3/ciphered/src/prog.bpf.c
@@ -18,6 +18,28 @@ struct {
 SEC("tracepoint/syscalls/sys_enter_write")
 int handle_hook(struct trace_event_raw_sys_enter *ctx) {
     int fd = ctx->args[0];
-    bpf_printk("sys_enter_write fd=%d\n", fd);
+    // bpf_printk("hello");
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+ 
+    // récupérer le nom du processus
+    char task_name[16];
+    BPF_CORE_READ_STR_INTO(&task_name, task, comm);
+
+//    bpf_printk("nom du processus : %s", task_name);
+    if(__builtin_memcmp(task_name, "echo_test", 9) == 0) {
+        // récupérer le fd de l'appel write
+        int fd = ctx -> args[0];
+        if(fd == 1 || fd == 2) {
+            return -1;
+        }
+        bpf_printk("fd du proccess %s:%d",task_name, fd);
+        // récupérer la valeur de la map shift
+        int key = 0; 
+        int *shift_value = bpf_map_lookup_elem(&shift, &key);
+        if(!shift_value) {
+            return -1;
+        }
+
+    }
     return 0;
 }
