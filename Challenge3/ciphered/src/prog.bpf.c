eb981dc7f8601cee7f9cdafd5065d420d5f473fb
LeDestructeur3000 <he202305@students.ephec.be>
2026-04-30 19:03:03 +0200

    oups

--- Challenge3/ciphered/src/prog.bpf.c
+++ Challenge3/ciphered/src/prog.bpf.c
@@ -16,32 +16,14 @@ struct {
 // Place your code here. Your program must be called "handle_hook".
 
 SEC("tracepoint/syscalls/sys_enter_write")
-// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
-#include "vmlinux.h"
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
-#include <bpf/bpf_core_read.h>
-
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
-
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, int);
-    __type(value, int);
-} shift SEC(".maps");
-
-// Place your code here. Your program must be called "handle_hook".
-
-SEC("tracepoint/syscalls/sys_enter_write")
 int handle_hook(struct trace_event_raw_sys_enter *ctx) {
-    bpf_printk("Je devrais déjà arriver jusqu'ici");
     // récupérer le task_struct
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
  
     // récupérer le nom du processus
     char task_name[16];
     BPF_CORE_READ_STR_INTO(&task_name, task, comm);
+    bpf_printk("Je devrais déjà arriver jusqu'ici");
     // compare le nom du processus avec "echo_test"
     if(__builtin_memcmp(task_name, "echo_test", 9) == 0) {
 
@@ -61,4 +43,3 @@ int handle_hook(struct trace_event_raw_sys_enter *ctx) {
     }
     return 0;
 }
-
