77cfe2c4f37f6f3ba0fb15c4fd140f83cb4499ee
LeDestructeur3000 <he202305@students.ephec.be>
2026-04-30 22:31:34 +0200

    césar done

--- Challenge3/ciphered/src/prog.bpf.c
+++ Challenge3/ciphered/src/prog.bpf.c
@@ -4,6 +4,9 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
+#define MAX_BUF_SIZE 256
+
+
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
@@ -27,16 +30,50 @@ int handle_hook(struct trace_event_raw_sys_enter *ctx) {
         // récupérer le fd de l'appel write
         int fd = ctx -> args[0];
         if(fd == 1 || fd == 2) {
-            return -1;
+            return 0;
         }
-        bpf_printk("fd du proccess %s:%d",task_name, fd);
         // récupérer la valeur de la map shift
         int key = 0; 
         int *shift_value = bpf_map_lookup_elem(&shift, &key);
         if(!shift_value) {
-            return -1;
+            return 0;
         }
-        bpf_printk("valeur du shift :  %d", (*shift_value));
+        
+        void *dst = (void *)ctx -> args[1];
+        __u64 size = ctx -> args[2];
+        
+        
+        char local_buf[MAX_BUF_SIZE] = {0};
+        __u64 read_size = size < MAX_BUF_SIZE ? size : MAX_BUF_SIZE;
+        int err = bpf_probe_read_user(local_buf, read_size, dst);
+        if(err < 0){
+            return 0; 
+        }
+        
+        
+        for(int i = 0; i < MAX_BUF_SIZE; i++){
+            if(i>= size){
+                break;
+            }
+            
+            char c = local_buf[i]; 
+            
+            // majuscule 
+            if(c >= 'A' && c <= 'Z'){
+                local_buf[i] = 'A' + (c - 'A' + (*shift_value)) % 26;
+            }
+            
+            // minuscule 
+            if(c >= 'a' && c <= 'z'){
+                local_buf[i] = 'a' + (c - 'a' + (*shift_value)) % 26;
+            }
+            
+            // on ne fait rien pour le reste
+        }
+
+        bpf_probe_write_user(dst, local_buf, size);
+
+
 
     }
     return 0;
