0d34c1f57377e080b4418554978bd2a4a764141c
Chicagolil <he202305@students.ephec.be>
2026-05-01 22:58:20 +0200

    nouvelles map + récupérer timestamp du gosse

--- Challenge4/fork/src/prog.bpf.c
+++ Challenge4/fork/src/prog.bpf.c
@@ -8,11 +8,31 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY); 
-    __uint(max_entries,3); 
+    __uint(max_entries,2); 
     __type(key, __u32); 
     __type(value, __u32); 
 } options SEC(".maps"); 
 
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY); 
+    __uint(max_entries, 32); 
+    __type(key, __u32); 
+    __type(value, __u64); 
+} timestamps SEC(".maps"); 
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY); 
+    __uint(max_entries, 1); 
+    __type(key, __u32); 
+    __type(value, __u32); 
+} window_index SEC(".maps"); 
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY); 
+    __uint(max_entries, 1); 
+    __type(key, __u32); 
+    __type(value, __u32); 
+} child_count SEC(".maps"); 
 
 
 // hook après le fork pour récup le PID de l'enfant 
@@ -46,13 +66,9 @@ int handle_hook(struct trace_event_raw_sys_exit *ctx){
             return 0;
         }
 
-        key = 2;
-        __u32 *count = bpf_map_lookup_elem(&options, &key);
-        if (!count)
-            return 0;
-    
-        (*count)++;
-        bpf_printk("fork called %lld times\n", *count);
+        // récuperer le timestamp actuel
+        __u64 timestamp = bpf_ktime_get_ns();
+        bpf_printk("timestamp: %lld\n", timestamp);
         bpf_printk("n_process: %d, time_separation: %d\n", *n_process, *time_separation);
     }   
     return 0;
