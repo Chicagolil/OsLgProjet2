eb128c847f857161f54751a8558861b1baf92ea1
Chicagolil <he202305@students.ephec.be>
2026-05-02 20:56:56 +0200

    est ce que le hook hook ?

--- Challenge5/page_faults/src/prog.bpf.c
+++ Challenge5/page_faults/src/prog.bpf.c
@@ -7,3 +7,30 @@
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 // Place your code here. Your program must be called "handle_hook".
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY); 
+    __uint(max_entries, 1); 
+    __type(key, __u32); 
+    __type(value, __u32); 
+} counter SEC(".maps"); 
+
+
+SEC("kprobe/handle_mm_fault")
+BPF_KPROBE(handle_hook){
+
+    struct task_struct *task= (struct task_struct *)bpf_get_current_task();
+    
+    char task_name[16]; 
+    BPF_CORE_READ_STR_INTO(&taskname, task, comm); 
+
+    if(__builtin_memcmp(task_name, "page_fault_gen")){
+        __u32 key = 0;
+        __u32 *count = bpf_map_lookup_elem(&counter, &key ); 
+        if(!count){
+            return 0; 
+        }
+
+        bpf_printk("Nbr de fois que le hook est éxécuté : %d"; *count); 
+    }
+}
