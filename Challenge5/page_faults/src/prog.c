4ca5ebdb2a5df1663f256fa5e27924c9bb78ca3f
Chicagolil <he202305@students.ephec.be>
2026-05-02 21:53:02 +0200

    options suite

--- Challenge5/page_faults/src/prog.c
+++ Challenge5/page_faults/src/prog.c
@@ -10,6 +10,13 @@ static void handle_sig(int sig) {
     running = 0;
 }
 
+static struct option long_options[] = {
+    {"lower_bound_freq_ms",required_argument, 0,'l'},
+    {"upper_bound_freq_ms", required_argument, 0, 'u'},
+    {"time_window_ms", required_argument, 0, 't'},
+    {0,0,0,0}
+};
+
 int main(void) {
     struct bpf_object *obj;
     struct bpf_program *prog;
@@ -29,6 +36,49 @@ int main(void) {
         bpf_object__close(obj);   // clean up on error
         return 1;
     }
+
+    // parser les arguments 
+    int opt; 
+    int lower_bound_freq_ms = 10; //défaut
+    int upper_bound_freq_ms = 100; //défaut 
+    int time_window_ms = 50; //défaut 
+
+    while((opt = getopt_long(argc, argv,"u:l:t:",long_options, NULL)) != -1){
+        switch(opt){
+            case 'l' : 
+                lower_bound_freq_ms = atoi(optarg); 
+                break;
+            case 'u' : 
+                upper_bound_freq_ms = atoi(optarg); 
+                break; 
+            case 't' : 
+                time_window_ms = atoi(optarg); 
+                break; 
+        }
+    }
+
+    // map des options 
+    struct bpf_map *options = bpf_object__find_map_by_name(obj, "options");
+    if(!options){
+        fprintf(stderr, "map not found\n");
+        bpf_object__close(obj); 
+        return -1; 
+    }
+
+
+    __u32 key = 0; 
+    __u32 val = (__u32)lower_bound_freq_ms; 
+    bpf_map__update_elem(options, &key, sizeof(key), &val, sizeof(val), BPF_ANY);
+
+    key = 1;
+    val = (__u32)upper_bound_freq_ms;
+    bpf_map__update_elem(options, &key, sizeof(key), &val, sizeof(val), BPF_ANY); 
+
+    key = 2;
+    val = (__u32)time_window_ms;
+    bpf_map__update_elem(options, &key, sizeof(key), &val, sizeof(val), BPF_ANY); 
+
+    
 
     // Find the BPF program by name
     prog = bpf_object__find_program_by_name(obj, "handle_hook");
